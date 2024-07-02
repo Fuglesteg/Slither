b182ac1499f702bb4b012cd34560d12d61374fa7
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2024-07-02 22:37:01 +0200

    Add support for multiple circles in circle.glsl
    colors are still a bit off

--- circle.glsl
+++ circle.glsl
@@ -13,7 +13,8 @@ struct Circle {
 
 Circle circles[] = Circle[](
     Circle(vec2(1, 0.5), 0.2, vec3(0.8, 0.2, 0.8)),
-    Circle(vec2(0, 0), 0.2, vec3(0.0, 0.5, 1)));
+    Circle(vec2(0, 0), 0.2, vec3(0.0, 0.5, 1)),
+    Circle(vec2(0.5, 0.7), 0.2, vec3(0.2, 0.2, 0.3)));
 
 float smin(float a, float b, float k) {
     float h = clamp(0.5+0.5*(b-a)/k, 0.0, 1.0);
@@ -35,18 +36,17 @@ void main() {
     vec2 uv = gl_FragCoord.xy / vec2(2000, 2000);
 
     circles[1].position = position;
-    vec3 finalColor = vec3(0, 0, 0);
-    float result = 0;
-    float distances[circles.length()];
-    float lastDistance = 0;
+    vec3 finalColor = circles[0].color;
+    float lastDistance = sdCircle(uv, circles[0].radius, circles[0].position);
+    float result = lastDistance;
 
-    for (int i = 0; i < circles.length(); i++) {
+    for (int i = 1; i < circles.length(); i++) {
         float distance = sdCircle(uv, circles[i].radius, circles[i].position);
-        finalColor = mix(finalColor, circles[i].color, lastDistance - distance);
-        distances[i] = distance;
+        finalColor = mix(circles[i].color, finalColor, distance - lastDistance);
+        result = smin(result, distance, 0.2);
         lastDistance = distance;
-        result = smin(result, distance, 0.8);
     }
+
     result = step(0., result);
     finalColor = mix(finalColor, vec3(0.0, 0.0, 0.0), result);
     FragColor = vec4(finalColor, 1.0);
