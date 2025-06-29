1e63db0ff85bf720fbaa0e64c73b8b71e2fdedf8
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-06-29 18:58:37 +0200

    Add script for loading system in a guix shell

---
+++ run-micros-server.lisp
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S guix shell -m manifest.scm -- bash -c 'LD_LIBRARY_PATH=${LIBRARY_PATH} sbcl --script $0'
+
+(require 'asdf)
+
+(pushnew :dev *features*)
+
+(asdf:load-system :micros)
+(micros:create-server :dont-close t)
+
+(asdf:load-asd (merge-pathnames #P"slither.asd" (uiop:getcwd)))
+(asdf:load-system :slither :force t)
+
+(read)
