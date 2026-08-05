6a1fc21432983f6962f621bdc2f194443a3a4430
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2026-08-05 18:48:32 +0200

    UI: Fix :grow sizing mode for layout directions

--- examples/ui.lisp
+++ examples/ui.lisp
@@ -9,6 +9,9 @@
    (ui-layout
      (box (:layout-direction :top-to-bottom
            :background-color (vec4 1.0 0.5 0.3 1.0))
+       (box (:height 10.0
+             :width :grow
+             :background-color (vec4 1.0 0.0 0.0 1.0)))
        (box (:padding-left 10.0 :padding-right 10.0
              :padding-top 10.0 :padding-bottom 10.0
              :child-gap 10.0)
@@ -20,7 +23,10 @@
              :child-gap 10.0)
          (loop repeat 10
                collect (box (:height 100.0 :width 100.0
-                             :background-color (vec4 0.1 0.4 0.7 1.0)))))))))
+                             :background-color (vec4 0.1 0.4 0.7 1.0)))))
+       (box (:height 10.0
+             :width :grow
+             :background-color (vec4 1.0 0.0 0.0 1.0)))))))
 
 (defun start-example ()
   (setf (current-scene) (make-instance 'ui-example))
